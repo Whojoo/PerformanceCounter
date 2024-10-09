69ab846f86b40e253612a9b0e21f1b8e0f1fbb3c
Whojoo <de.gier.robin@gmail.com>
2024-10-09 14:07:44 +0200

    docs: Add documentation to the factory

--- src/Whojoo.PerformanceCounter/PerformanceCounterFactory.cs
+++ src/Whojoo.PerformanceCounter/PerformanceCounterFactory.cs
@@ -4,11 +4,24 @@ namespace Whojoo.PerformanceCounter;
 
 public static class PerformanceCounterFactory
 {
+    /// <summary>
+    /// Create and start a default performance counter. It wil report its results as LogLevel Information.
+    /// </summary>
+    /// <param name="logger">Logger used for reporting</param>
+    /// <param name="performanceCounterName">Name used as the header in the report</param>
+    /// <returns>A new and running performance counter</returns>
     public static IPerformanceCounter StartDefault(ILogger logger, string performanceCounterName)
     {
         return new Implementation.PerformanceCounter(logger, performanceCounterName);
     }
 
+    /// <summary>
+    /// Create a performance counter which reports its results as a specific LogLevel.
+    /// </summary>
+    /// <param name="logLevel">LogLevel used for performance reporting</param>
+    /// <param name="logger">Logger used for reporting</param>
+    /// <param name="performanceCounterName">Name used as the header in the report</param>
+    /// <returns>A new and running performance counter</returns>
     public static IPerformanceCounter StartWithLogLevel(LogLevel logLevel, ILogger logger, string performanceCounterName)
     {
         return new Implementation.PerformanceCounter(logger, performanceCounterName, logLevel);
