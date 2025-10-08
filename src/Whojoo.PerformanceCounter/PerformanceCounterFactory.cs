591eb91c02242711428d573fe4047b222424bc88
Robin de Gier <de.gier.robin@gmail.com>
2025-10-08 22:42:48 +0200

    feat: Remove deprecated api
    BREAKING-CHANGE: StartDefault and StartWithLogLevel are no longer available

--- src/Whojoo.PerformanceCounter/PerformanceCounterFactory.cs
+++ src/Whojoo.PerformanceCounter/PerformanceCounterFactory.cs
@@ -25,29 +25,4 @@ public static class PerformanceCounterFactory
 
         return new Implementation.PerformanceCounter(logger, options.PerformanceCounterName, options.LogLevel);
     }
-
-    /// <summary>
-    /// Create and start a default performance counter. It wil report its results as LogLevel Information.
-    /// </summary>
-    /// <param name="logger">Logger used for reporting</param>
-    /// <param name="performanceCounterName">Name used as the header in the report</param>
-    /// <returns>A new and running performance counter</returns>
-    [Obsolete("This method is obsolete and will be removed in v2, use Start instead.")]
-    public static IPerformanceCounter StartDefault(ILogger logger, string performanceCounterName)
-    {
-        return new Implementation.PerformanceCounter(logger, performanceCounterName);
-    }
-
-    /// <summary>
-    /// Create a performance counter which reports its results as a specific LogLevel.
-    /// </summary>
-    /// <param name="logLevel">LogLevel used for performance reporting</param>
-    /// <param name="logger">Logger used for reporting</param>
-    /// <param name="performanceCounterName">Name used as the header in the report</param>
-    /// <returns>A new and running performance counter</returns>
-    [Obsolete("This method is obsolete and will be removed in v2, use Start instead.")]
-    public static IPerformanceCounter StartWithLogLevel(LogLevel logLevel, ILogger logger, string performanceCounterName)
-    {
-        return new Implementation.PerformanceCounter(logger, performanceCounterName, logLevel);
-    }
 }
